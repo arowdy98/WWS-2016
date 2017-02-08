b22691ddacced240a45ef9efcd414faf8aabc6f8
arowdy98 <arathore98@gmail.com>
2017-02-08 15:22:42 +0530

    Add files via upload
    Force sensitive resistor

---
+++ fsr.ino
@@ -0,0 +1,13 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(A0,INPUT);
+pinMode(22,OUTPUT);
+digitalWrite(22,HIGH);
+Serial.begin(9600);
+}
+int weight;
+void loop() {
+  // put your main code here, to run repeatedly:
+  weight=analogRead(A0);
+  Serial.println(weight);
+}
